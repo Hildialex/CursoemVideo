d555b112a7479e4b8c2630a9238b685e0521b548
Hildialex <hildialex@hotmail.com>
2021-05-16 11:43:02 -0300

    Corrigidos com Guanabara

--- Python/Exercicios/ex062.py
+++ Python/Exercicios/ex062.py
@@ -13,3 +13,20 @@ while n <= (decimo+razao):
         else:
             z += novo
             decimo = termo + (10 + z - 1) * razao
+#*SOLUÇÃO GUANABARA
+# primeiro = int(input('Digite o primeiro termo:'))
+# razao = int(input('Digite a razão:'))
+# termo = primeiro
+# cont = 1
+# total = 0
+# mais = 10
+# while mais != 0:
+#   total += mais
+#   while cont <= total:
+#       print('{} ¬'.format(termo), end =' ')
+#       termo += razao
+#       cont += 1
+#   print('PAUSA')
+#   mais = int(input('Quantos termos quer mostrar a mais?'))
+# print('Progressão finalizada com {} termos'.format(total))
+# #
--- Python/Exercicios/ex063.py
+++ Python/Exercicios/ex063.py
@@ -7,4 +7,19 @@ while n < num:
     a = b
     b = fibo
     n += 1
-
+print('FIM')
+#*SOLUÇÃO GUANABARA
+#  n = int(input('Qunatos termos voce quer mostar?'))
+#  t1 = 0
+#  t2 = 1
+#  print('~'*30)
+#  print('{} ¬ {}'.format(t1, t2), end =' ')
+#  cont = 3
+#  while cont <= n:
+#   t3 = t1 + t2
+#   print('¬ {}'.format(t3), end =' ')
+#   t1 = t2
+#   t2 = t3
+#   cont += 1
+# print('FIM')
+# #
