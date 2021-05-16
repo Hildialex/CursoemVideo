9a7cd258dcfee449fd3a8bde9713315993618a80
Hildialex <hildialex@hotmail.com>
2021-05-16 11:16:49 -0300

    Concertando o Fibonaci
    Lembrei da lógica

--- Python/Exercicios/ex063.py
+++ Python/Exercicios/ex063.py
@@ -1,7 +1,10 @@
 num = int(input('Digite um número:'))
-n = 0
-f = 1
+a = fibo = 0
+b = n = 1
 while n < num:
+    fibo = a + b
+    print('{} ¬ {}'.format(b,fibo) if fibo == 1 else '{}'.format(fibo), end=' ¬ ')
+    a = b
+    b = fibo
     n += 1
-    print('{}'.format(f), end=' ¬ ')
-    f = (f + n) - f
+
